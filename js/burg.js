0d429fa8e902030447cb69bb97350079adbffbda
Oleg Pavlish <olegpa2005@gmail.com>
2024-01-08 19:16:00 +0200

    fixes + logs/sign ups

--- js/burg.js
+++ js/burg.js
@@ -1,7 +1,33 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header__menu');
+let connect = document.querySelector('.header__nav-list-f');
+let info = document.querySelector('.header__nav-list-info');
+let contact = document.querySelector('.header__nav-list-contact');
+let log = document.querySelector('.header__nav-list-log');
+let sign = document.querySelector('.header__nav-list-sign');
+
 
 menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+connect.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+info.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+contact.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+log.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+sign.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
--- js/logIn.js
+++ js/logIn.js
@@ -1,9 +1,35 @@
 document.getElementById('showLoginFormBtn').addEventListener('click', function() {
-    var loginForm = document.getElementById('loginForm');
+    let loginForm = document.getElementById('loginForm');
     loginForm.classList.toggle('hidden__logIn');
-  });
-document.getElementById('hideLogInshowSignIn').addEventListener('click', function() {
-    var loginForm = document.getElementById('loginForm');
+    console.log('Show login form clicked');
+});
+
+document.getElementById('hideLogInshowSignUp').addEventListener('click', function(event) {
+    event.preventDefault(); // Щоб заборонити перехід за посиланням
+    console.log('Hide login form and show sign-in clicked');
+    let loginForm = document.getElementById('loginForm');
     loginForm.classList.toggle('hidden__logIn');
-    /*var signinForm = document.getElementById('')*/
-  });
+    console.log('Show sign up');
+    let signupform = document.getElementById('signupForm');
+    signupform.classList.toggle('hidden__sign-up');
+    // Додайте інші дії, якщо потрібно
+});
+document.getElementById('hideSignUpshowLogIn').addEventListener('click', function() {
+    let signup = document.getElementById('signupForm');
+    signup.classList.toggle('hidden__sign-up');
+    console.log('Hide Sign Up');
+    let login = document.getElementById('loginForm');
+    login.classList.toggle('hidden__logIn');
+    console.log('Show Log In')
+});
+
+document.getElementById('SignUpToConnect').addEventListener('click', function() {
+    let signup = document.getElementById('signupForm');
+    signup.classList.toggle('hidden__sign-up');
+    console.log('Show login form clicked');
+});
+document.getElementById('ShowSignUpFromHeader').addEventListener('click', function() {
+    let signup = document.getElementById('signupForm');
+    signup.classList.toggle('hidden__sign-up');
+    console.log('Show login form clicked');
+});
